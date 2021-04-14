0405070e01d870e825104a6c921f6371e867d816
Prabhjot Dev <prabhjotdev@live.com>
2021-04-14 10:00:14 -0400

    Added all calculator functions
    Subtract, Multiply, and divided added to app.js, and test.js.

--- CalculatorApp/app.js
+++ CalculatorApp/app.js
@@ -1,25 +1,29 @@
-const sum = (a,b) => {
+ const sum = (a,b) => {
     const result = a + b
     return result
 }
 
-// const diff = (a,b) => {
-//     const result = a - b
-//     return result
-// }
+ const subtract = (a,b) => {
+    const result = a - b
+    return result
+}
 
-// const multiply = (a,b) => {
-//     const result = a * b
-//     return result
-// }
+const multiply = (a,b) => {
+    const result = a * b
+    return result
+}
 
-// const divide = (a,b) => {
-//     const result = a / b
-//     return result
-// }
+const divide = (a,b) => {
+    const result = a / b
+    return result
+}
 
 //manual testing below
 // console.log(`The sum of the number is: `)
 // console.log(sum(25,100))
 
-module.exports = sum
+module.exports.sum = sum;
+module.exports.subtract = subtract;
+module.exports.multiply = multiply;
+module.exports.divide = divide;
+
--- CalculatorApp/test.js
+++ CalculatorApp/test.js
@@ -1,25 +1,29 @@
-const sum = require("./app")
- 
+const {sum, subtract, multiply, divide} = require("./app")
+
+
 describe("Adding two numbers", () => {
    it("Should return the sum", ()=> {
        expect(sum(9,5)).toBe(14)               
    })
-})
+   it("Adding larger numbers", ()=> {
+    expect(sum(965465,56565)).toBe(1022030)               
+   })
+})  
 
-// describe("Subtract two numbers", () => {
-//     it("Should return the difference", ()=> {
-//         expect(diff(10,5)).toBe(5)               
-//     })
-//  })
+describe("Subtract two numbers", () => {
+    it("Should return the difference", ()=> {
+        expect(subtract(10,5)).toBe(5)               
+    })
+ })
 
-//  describe("Multiply two numbers", () => {
-//     it("Should return the product", ()=> {
-//         expect(multiply(10,5)).toBe(50)               
-//     })
-//  })
+ describe("Multiply two numbers", () => {
+    it("Should return the product", ()=> {
+        expect(multiply(10,5)).toBe(50)               
+    })
+ })
 
-//  describe("Divide two numbers", () => {
-//     it("Should return the division", ()=> {
-//         expect(divide(10,5)).toBe(2)               
-//     })
-//  })
+ describe("Divide two numbers", () => {
+    it("Should return the division", ()=> {
+        expect(divide(10,5)).toBe(2)               
+    })
+ })
